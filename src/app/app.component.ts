efd15d480283b941a3f46313aa5ad3f4ab139ae4
Nigin N Manayil <niginnmanayil10@gmail.com>
2024-04-01 11:46:54 +0530

    Feat : Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MyFirstAngularApp';
+  public msg = 'Hello World';
+
+  onchange(val : any) {
+    this.msg = val;
+  }
+
+  public fruits = [
+    {name : 'Apple', price : 50},
+    {name : 'Apple', price : 80},
+    {name : 'Apple', price : 120},
+    {name : 'Apple', price : 150},
+    {name : 'Apple', price : 20},
+  ]
 }
+
